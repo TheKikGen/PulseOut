7f9ac7e6f037b15cba4576fbd8e1bee5dffcee17
Franck <ftouanen@gmail.com>
2018-03-20 23:16:17 +0100

    Changed the max pin to 50 for STM32Duino compatibility

--- PulseOutManager.h
+++ PulseOutManager.h
@@ -9,18 +9,18 @@
 #include <Arduino.h>
 #include "PulseOut.h"
 
-#define PULSEOUT_MAX_PIN  20 // For 20 pins from 0 to 19
+#define PULSEOUT_MAX_PIN  50 // For 40 pins from 0 to 39
 class PulseOutManager
   {
     private:
-     
+
     public:
       PulseOutManager();
       uint8_t getCount();
       PulseOut* factory( uint8_t pin, unsigned long duration, uint8_t pulse=HIGH, bool square=false );
       bool add(PulseOut* pulse);
       bool remove(PulseOut* pulse);
-      void clear();            
+      void clear();
       void begin();
       void startAll();
       void update(unsigned long currentMillis);
@@ -29,11 +29,7 @@ class PulseOutManager
       // Class Member Variables
       PulseOut* _pulses[PULSEOUT_MAX_PIN];
       bool      _mustStartAll = false ;
-      uint8_t   _pulseCounter = 0     ;       // the # of pulse currently active 
+      uint8_t   _pulseCounter = 0     ;       // the # of pulse currently active
   };
 
 #endif
-
-
-
-
